64221436679749b7f58860aefefa83f3ae0debe8
Levi Smith <smith.levim@gmail.com>
2016-12-01 16:18:06 -0700

    added migrations and changed models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,16 @@
 class Project < ApplicationRecord
+	after_initialize :set_defaults
+
+	has_many :tasks
+
+	validates_presence_of :title, :description, :percent_complete
+	
+	scope :almost_completed, -> {where('percent_complete > 75.0')}
+	scope :still_needs_more_work, -> { where('percent_complete <75.0') }
+
+	def set_defaults
+		self.percent_complete ||= 0.0
+	end
+
+
 end
